a53a8e4c83e241e91cd73cc4a5a20cff0219c13c
michaelward82 <m.ward@leythers.com>
2015-01-10 19:41:36 +0000

    Initial version of LISA theme

---
+++ public/wp-content/themes/lisa/front-page.php
@@ -0,0 +1,13 @@
+<div id="container" class="fade-in">
+	<div id="big-message">
+		<p id="big-primary"><strong>Together We Make <em>Leigh Centurions</em> Stronger</strong></p>
+		<div class="btn-group fade-in-late">
+			<a class="btn info">Find out more</a>
+			<span class="btn-additional">
+				<a class="btn cta pulse">Join today</a>
+				<div class="btn-info">&amp; choose your monthly donation</div>
+			</span>
+		</div>
+		<p id="big-secondary" class="fade-in-late"><strong>LISA uses your donations to secure the contracts of <em>key senior players</em> &amp; funds full-time playing contracts for <em>young stars</em> of the future</strong></p>
+	</div>
+</div>
---
+++ public/wp-content/themes/lisa/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="pan">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<header class="fade-in-late">
+		<h1>Leigh Independant <br>Supporters Association</h1>
+		<div id="header-btns">
+			<a class="btn info">Find out more</a>
+			<a class="btn info">Latest news</a>
+			<a class="btn cta inverted">Join today</a>
+		</div>
+	</header>
