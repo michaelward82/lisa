af3839527fccbc7bb9f90e8bf3cb454e23a80e68
michaelward82 <m.ward@leythers.com>
2015-01-10 23:35:43 +0000

    Tries different page content syntax

--- public/wp-content/themes/lisa/page.php
+++ public/wp-content/themes/lisa/page.php
@@ -2,7 +2,7 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-	<?php get_template_part( 'content', 'page' ); ?>
+	<?php get_template_part( 'loop', 'page' ); ?>
 
 <?php endwhile; // end of the loop. ?>
 
