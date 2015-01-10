e60716d85d1c8f3521d5bc40e0e823ca069ca4ac
michaelward82 <m.ward@leythers.com>
2015-01-10 23:38:57 +0000

    Adds page template comment. Reverts post content code changes

--- public/wp-content/themes/lisa/page.php
+++ public/wp-content/themes/lisa/page.php
@@ -1,8 +1,14 @@
+<?php
+/*
+Template Name: Page
+*/
+?>
+
 <?php get_header(); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-	<?php get_template_part( 'loop', 'page' ); ?>
+	<?php get_template_part( 'content', 'page' ); ?>
 
 <?php endwhile; // end of the loop. ?>
 
